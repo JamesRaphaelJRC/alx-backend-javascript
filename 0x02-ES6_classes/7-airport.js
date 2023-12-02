722ea6ff4aca2dec852e2b39f4b3c72c92165025
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2023-12-02 13:15:02 +0100

    task 7

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,33 @@
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (!(typeof name === 'string') && !(name instanceof String)) {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if (!(typeof code === 'string') && !(code instanceof String)) {
+      throw new Error('Code must be a string');
+    }
+    this._code = code;
+  }
+
+  // String representation for Airport instances
+  toString() {
+    return `[${typeof (Object.getPrototypeOf(this))} ${this._code}]`;
+  }
+}
