7582b5de1dbe316202eec03d6d3d16f3d7b9d087
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2023-12-02 04:39:57 +0100

    task 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,36 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  // Getter for code
+  get code() {
+    return this._code;
+  }
+
+  // Setter for code
+  set code(newCode) {
+    if ((typeof newCode !== 'string') && !(newCode instanceof String)) {
+      throw new Error('Code must be a string');
+    }
+    this._code = newCode;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Setter for name
+  set name(newName) {
+    if ((typeof newName !== 'string') && !(newName instanceof String)) {
+      throw new Error('Name must be a string');
+    }
+    this._code = newName;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`
+  }
+}
