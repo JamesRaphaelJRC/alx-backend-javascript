ead7f33a37158693e91e72fe2b09439b265c84cd
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2023-12-02 12:52:20 +0100

    task 6

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,32 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    // Calls the constructor of the parent
+    super(sqft);
+
+    this.floors = floors;
+  }
+
+  // Getter for sqft
+  get sqft() {
+    return this._sqft;
+  }
+
+  // Getter and Setter for floors
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(floors) {
+    if ((typeof floors !== 'number') && !(floors instanceof Number)) {
+      throw new TypeError('Floor must be a number');
+    }
+    this._floors = floors;
+  }
+
+  // Overriding the method named evacuationWarningMessage
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
