442b7078a77b7cdb60f99519ac7a8745f37f361f
jamesrapjaeljrc <jamesraphaeljrc@gmail.com>
2023-12-05 15:25:57 +0100

    task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   const matchingElements = [];
-  if (!(set instanceof Set)) {
+  if (!(set instanceof Set) && typeof startString !== 'string') {
     return '';
   }
   set.forEach((element) => {
