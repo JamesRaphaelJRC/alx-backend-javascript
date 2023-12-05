c23128b283cb2c93b70387d71900d73298eef8d9
jamesrapjaeljrc <jamesraphaeljrc@gmail.com>
2023-12-05 07:23:52 +0100

    task 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,11 +1,11 @@
+import getListStudentIds from './1-get_list_student_ids';
+
 /**
  * function getStudentIdsSum: Gets the sum of all student ids
  * listOfStudents - A list of objects containing students information
  *                  see '0-getListStudents.js'
  * Return: Sum of all student ids on success
  */
-import getListStudentIds from './1-get_list_student_ids';
-
 export default function getStudentIdsSum(listofStudents) {
   if (!Array.isArray(listofStudents)) {
     throw new TypeError('List of students must be an array');
