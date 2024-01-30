c4234990b86fd498ed3b3d27dcfa036ab93e7a5d
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2024-01-30 22:13:50 +0100

    task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,16 +1,19 @@
 const http = require('http');
 
+const hostname = '127.0.0.1';
+const port = 1245;
+
 // Create a small HTTP server
 const app = http.createServer((req, res) => {
   // Set the response headers
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.setHeader(200, {'Content-Type': 'text/plain'});
+  res.statusCode = 200;
 
   // Display 'Hello Holberton School!' in the page body for any endpoint
-  res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!');
 });
 
 // HTTP server should listen on port 1245
-const port = 1245;
 app.listen(port, () => {
 });
 
